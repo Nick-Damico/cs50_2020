1eaecdd60e28c3648a42c77d9bc326ad1992bded
Nick-Damico <nickalan82@icloud.com>
2020-08-21 08:13:08 -0400

    WIP: Add helpers. Encrypt plain text user supplied word.

--- pset2/substitution/substitution.c
+++ pset2/substitution/substitution.c
@@ -2,8 +2,14 @@
 #include <cs50.h>
 #include <string.h>
 
+// Constants
+#define MAX_LENGTH 10
+
 // Declare functions for use in main.
+string encrypt_text(string plain_text, string key);
+int calc_letter_offset(char letter);
 int valid_key(string key);
+int valid_arg_length(int length);
 
 int main(int argc, string argv[])
 {
@@ -11,7 +17,7 @@ int main(int argc, string argv[])
     //  1. Make sure only 1 argument is provided.
     //  2. if more than (1) arg provided error msg.
     //     and return (1) from main.
-    if (argc > 2 || argc < 2)
+    if (!valid_arg_length(argc))
     {
         printf("Error: provide 1 argument with program call.\n");
         return 1;
@@ -20,8 +26,19 @@ int main(int argc, string argv[])
 
     if (!valid_key(key))
     {
-        printf("Error: Key not valid\n");
+        printf("Key must contain 26 characters.\n");
     }
+
+    //  Prompt user for "Plaintext: "
+    //  [√] 1. User will provide Plaintext for encryption.
+    string plain_text = get_string("Plaintext: ");
+
+    //  Encipher
+    string encrypted_text = encrypt_text(plain_text, key);
+    //  [] 1. For each character, determine what letter it maps to.
+    //  [] 2. Preserve case.
+    //  [] 3. Leave non-alphabetic characters as-is.
+
     //  Encipher
     //  [] 1. For each character, determine what letter it maps to.
     //  [] 2. Preserve case.
@@ -33,6 +50,44 @@ int main(int argc, string argv[])
 }
 
 // Helper functions
+string encrypt_text(string plain_text, string key)
+{
+   printf("\n");
+   // Example: hello, world => jrssb, ybwsp
+   // 1. iterate over plain_text.
+   // 2. use key to convert plaint_text letters.
+   for (int i = 0, length = strlen(plain_text); i < length; i++)
+   {
+       // 1. find difference(n) between letter and key.
+       // h (104)
+       int letter_offset = calc_letter_offset(plain_text[i]);
+       printf("Character offset: %i\n", letter_offset);
+   }
+   return plain_text;
+}
+
+int calc_letter_offset(char letter)
+{
+    int match;
+    int lower_offset = 65;
+    int upper_offset = 97;
+
+    if (letter >= 65 && letter <= 90)
+    {
+       match = letter - lower_offset;
+    }
+    else if (letter >= 97 && letter <= 122)
+    {
+        match = letter - upper_offset;
+    }
+    else
+    {
+        match = 0;
+    }
+
+    return match;
+}
+
 int valid_key(string key)
 {
     //  valid Key
@@ -44,4 +99,9 @@ int valid_key(string key)
     printf("String Length: %i\n", length);
 
     return valid;
+}
+
+int valid_arg_length(int length)
+{
+    return length == 2  ? 1 : 0;
 }
