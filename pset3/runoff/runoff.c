6c5e451df0024814737268a7008d35aaf729ce56
Nick-Damico <nickalan82@icloud.com>
2020-09-22 08:16:25 -0400

    Feat: WIP working version of #is_tie(int min).

--- pset3/runoff/runoff.c
+++ pset3/runoff/runoff.c
@@ -195,8 +195,17 @@ int find_min(void)
 // Return true if the election is tied between all candidates, false otherwise
 bool is_tie(int min)
 {
-    // TODO
-    return false;
+    // presume there is a tie.
+    bool tied = true;
+    for (int i = 0; i < candidate_count; i++)
+    {
+        // If a single candidates vote count is NOT the
+        // there is no tie, return false.
+        if (candidates[i].votes != min)
+            not_tied = false;
+            break;
+    }
+    return not_tied;
 }
 
 // Eliminate the candidate (or candidates) in last place
